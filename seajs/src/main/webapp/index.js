24548f2cb2dc598207c41e77b35d9ec9dc263a8a
fu_leisen <fu_leisen@cdv.com>
2015-12-07 10:22:39 +0800

    调整目录结构

--- seajs/src/main/webapp/index.js
+++ seajs/src/main/webapp/index.js
@@ -1,7 +1,6 @@
-require('bootstrap');
-var test1 = require('./js/test1');
-var test2 = require('./js/test2');
-var compile = require('./handlebars/test.handlebars');
+var test1 = require('./base/js/test1');
+var test2 = require('./base/js/test2');
+var compile = require('./base/handlebars/test.handlebars');
 var tpl = compile(test1);
 console.log(test1.getName()); 
 console.log(test2.getName());
